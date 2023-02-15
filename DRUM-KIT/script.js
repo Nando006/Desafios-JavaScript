638c511d5acf98452f200e7fb539deb3d76eaa21
Nando006 <fsnando.012@outlook.com>
2023-02-15 18:56:02 -0300

    Att DRUM-KIT

--- DRUM-KIT/script.js
+++ DRUM-KIT/script.js
@@ -1,42 +1,60 @@
-'use strict'
+'use strict';
+
+const createSpan = (text) => {
+    const span = document.createElement('span');
+    span.classList.add('key');
+    span.textContent = text;
+    span.id = text;
+    document.querySelector('#container-letras').appendChild(span);
+}
 
 const sons = {
     'A': './sounds/boom.wav',
-    'B': './sounds/clap.wav',
-    'C': './sounds/hihat.wav',
-    'D': './sounds/kick.wav',
-    'E': './sounds/openhat.wav',
+    'D': './sounds/clap.wav',
+    'W': './sounds/hihat.wav',
+    'S': './sounds/kick.wav',
+    'C': './sounds/openhat.wav',
     'F': './sounds/ride.wav',
-    'G': './sounds/snare.wav',
-    'H': './sounds/tink.wav',
-    'I': './sounds/tom.wav'
+    'X': './sounds/snare.wav',
+    'Q': './sounds/tink.wav',
+    'Z': './sounds/tom.wav'
 }
 
-const criandoDiv = (texto) => {
-    const div = document.createElement('div');
-    div.classList.add('key');
-    div.textContent = texto;
-    div.id = texto;
-    document.querySelector('#container-letras').appendChild(div);
+const displayKeys = (sons) => {
+    Object.keys(sons).forEach(createSpan);
 }
 
-const exibirLetras = (sons) => {
-    Object.keys(sons).forEach(criandoDiv);
+displayKeys(sons);
+
+const keyLetter = (letter) => {
+    const audio = new Audio(`${sons[letter]}`);
+    audio.play();
 }
 
-exibirLetras(sons);
-
-const chamandoSom = (letra) => {
-    const pegando = new Audio(`${sons[letra]}`);
-    pegando.play()
+const effect = (letter) => {
+    document.getElementById(letter).classList.add('active');
 }
 
-const somAoClicar = (evento) => {
-    const letra = evento.target.id;
-    const validando = sons.hasOwnProperty(letra);
-    if(validando) {
-        chamandoSom(letra);
+const removeEffect = (letter) => {
+    const div = document.getElementById(letter);
+    const removeActive = () => div.classList.remove('active');
+    div.addEventListener('transitionend', removeActive);
+}
+
+const somDisplay = (evento) => {
+    let letter = '';
+    if(evento.type == 'click') {
+        letter = evento.target.id
+    } else {
+        letter = evento.key.toUpperCase()
+    }
+    const validation = sons.hasOwnProperty(letter);
+    if(validation) {
+        effect(letter);
+        keyLetter(letter);
+        removeEffect(letter);
     }
 }
 
-document.querySelector('#container-letras').addEventListener('click', somAoClicar);
+document.querySelector('#container-letras').addEventListener('click', somDisplay);
+window.addEventListener('keydown', somDisplay);
